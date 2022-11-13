153b4e8932cc1ce7a711045681d5c4168d1742e7
Fang Lyu <lyufang.cn@gmail.com>
2022-11-13 22:32:51 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -64,6 +64,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'drf_yasg',
+        'django_filters',
     ]
 
     SWAGGER_SETTINGS = {
@@ -193,6 +194,15 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend"
+        ],
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     LOGGING = {
